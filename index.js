8436f2adf2fa9ab11dd01397da00fca314a72412
stich86 <stich86@gmail.com>
2017-09-08 15:47:42 +0200

    first commit
    first commit

---
+++ index.js
@@ -0,0 +1,36 @@
+var exec = require("child_process").exec;
+var Service, Characteristic;
+
+module.exports = function(homebridge) {
+  Service = homebridge.hap.Service;
+  Characteristic = homebridge.hap.Characteristic;
+
+  homebridge.registerAccessory("homebridge-temperature-cmd", "TemperatureCmd", TemperatureCmdAccessory);
+}
+
+function TemperatureCmdAccessory(log, config) {
+  this.log = log;
+  this.name = config["name"];
+  this.cmd = config["cmd"];
+
+  this.service = new Service.TemperatureSensor(this.name);
+
+  this.service
+    .getCharacteristic(Characteristic.CurrentTemperature)
+    .on('get', this.getState.bind(this));
+}
+
+TemperatureCmdAccessory.prototype.getState = function(callback) {
+  var self = this;
+  exec(this.cmd, function (err, data) {
+    if (err) {
+      callback(err);
+      return
+    }
+    callback(null, parseFloat(data))
+  })
+}
+
+TemperatureCmdAccessory.prototype.getServices = function() {
+  return [this.service];
+}
